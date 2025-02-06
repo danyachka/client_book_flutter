119db5c0abf88ffda7641ce80115b82208dd8a3f
DanilaKisikov <114114788+DanilaKisikov@users.noreply.github.com>
2025-02-06 07:32:25 +0300

    Add CalendarWidget

--- lib/ui/pages/main_page/fragments/calendar_fragment.dart
+++ lib/ui/pages/main_page/fragments/calendar_fragment.dart
@@ -1,7 +1,11 @@
 
 
+import 'package:client_book_flutter/blocs/appointment_list/appointment_list_bloc.dart';
+import 'package:client_book_flutter/blocs/appointment_list/events/appointment_list_bloc_events.dart';
 import 'package:client_book_flutter/ui/pages/main_page/main_page.dart';
+import 'package:client_book_flutter/ui/widgets/calendar.dart/calendar_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CalendarFragment extends StatelessWidget {
 
@@ -13,6 +17,19 @@ class CalendarFragment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.yellow);
+    return SafeArea(bottom: false, child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8), 
+        child: Column(children: [
+          CalendarWidget(
+            onDatePicked:(pickedDate) async {
+              BlocProvider.of<MainAppointmentListBloc>(context).add(
+                ScrollToDateAppointmentListBlocEvent(time: pickedDate.millisecondsSinceEpoch)
+              );
+              changeFragmentCallBack(MainPageFragment.list);
+            },
+          )
+        ])
+      )
+    );
   }
 }
--- lib/utils/time_utils.dart
+++ lib/utils/time_utils.dart
@@ -13,6 +13,22 @@ extension AppDateTime on DateTime {
     return true;
   }
 
+  DateTime getNextMonthStart() {
+    if (month == 12) {
+      return DateTime(year + 1, 1);
+    }
+
+    return DateTime(year, month + 1);
+  }
+
+  DateTime getPreviousMonthStart() {
+    if (month == 1) {
+      return DateTime(year - 1, 12);
+    }
+
+    return DateTime(year, month - 1);
+  }
+
   String getMonthName(BuildContext context) {
     switch (month) {
       case 1:
@@ -44,47 +60,9 @@ extension AppDateTime on DateTime {
     }
   }
 
-  String getDayName(BuildContext context) {
-    switch (weekday) {
-      case 1:
-        return S.of(context).monday;
-      case 2:
-        return S.of(context).tuesday;
-      case 3:
-        return S.of(context).wednesday;
-      case 4:
-        return S.of(context).thursday;
-      case 5:
-        return S.of(context).friday;
-      case 6:
-        return S.of(context).saturday;
-      case 7:
-        return S.of(context).sunday;
-      default:
-        return ''; // Return an empty string or handle invalid input as needed
-    }
-  }
+  String getDayName(BuildContext context) => getDayNameByNumber(context, weekday);
 
-  String getDayShortName(BuildContext context) {
-    switch (weekday) {
-      case 1:
-        return S.of(context).monday_short;
-      case 2:
-        return S.of(context).tuesday_short;
-      case 3:
-        return S.of(context).wednesday_short;
-      case 4:
-        return S.of(context).thursday_short;
-      case 5:
-        return S.of(context).friday_short;
-      case 6:
-        return S.of(context).saturday_short;
-      case 7:
-        return S.of(context).sunday_short;
-      default:
-        return ''; // Return an empty string or handle invalid input as needed
-    }
-  }
+  String getDayShortName(BuildContext context) => getDayShortNameByNumber(context, weekday);
 
   Duration getTodayTimeInDuration() {
     return Duration(hours: hour, minutes: minute, seconds: second);
@@ -95,5 +73,47 @@ extension AppDateTime on DateTime {
 extension AppDuration on Duration {
   String getFormattedTime() {
     return "$inHours:$inMinutes";
+  }
+}
+
+String getDayNameByNumber(BuildContext context, int weekday) {
+  switch (weekday) {
+    case 1:
+      return S.of(context).monday;
+    case 2:
+      return S.of(context).tuesday;
+    case 3:
+      return S.of(context).wednesday;
+    case 4:
+      return S.of(context).thursday;
+    case 5:
+      return S.of(context).friday;
+    case 6:
+      return S.of(context).saturday;
+    case 7:
+      return S.of(context).sunday;
+    default:
+      return ''; // Return an empty string or handle invalid input as needed
+  }
+}
+
+String getDayShortNameByNumber(BuildContext context, int weekday) {
+  switch (weekday) {
+    case 1:
+      return S.of(context).monday_short;
+    case 2:
+      return S.of(context).tuesday_short;
+    case 3:
+      return S.of(context).wednesday_short;
+    case 4:
+      return S.of(context).thursday_short;
+    case 5:
+      return S.of(context).friday_short;
+    case 6:
+      return S.of(context).saturday_short;
+    case 7:
+      return S.of(context).sunday_short;
+    default:
+      return ''; // Return an empty string or handle invalid input as needed
   }
 }

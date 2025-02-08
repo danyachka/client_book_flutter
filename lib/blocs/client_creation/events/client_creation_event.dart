8c6d7abba03c193caaaceaeec2411f7f35863d9c
DanilaKisikov <114114788+DanilaKisikov@users.noreply.github.com>
2025-02-08 17:28:54 +0700

    Add ClientCreationBloc

---
+++ lib/blocs/client_creation/events/client_creation_event.dart
@@ -0,0 +1,9 @@
+
+
+class ClientCreationEvent {
+  final String name;
+
+  final String phone;
+
+  ClientCreationEvent({required this.name, required this.phone});
+}
---
+++ lib/blocs/client_creation/states/client_creation_state.dart
@@ -0,0 +1,36 @@
+
+
+import 'package:client_book_flutter/model/app_database.dart';
+
+abstract class ClientCreationState {}
+
+class DoneClientCreationState extends ClientCreationState {
+  final Client createdClient;
+
+  DoneClientCreationState({required this.createdClient});
+}
+
+class LoadingClientCreationState extends ClientCreationState {}
+
+class UnknownErrorClientCreationState extends ClientCreationState {}
+
+class CreationClientCreationState extends ClientCreationState {
+
+   final ClientCreationErrorType nameError;
+   final ClientCreationErrorType phoneError;
+
+   final String? unknownError;
+
+  CreationClientCreationState({required this.nameError, required this.phoneError, this.unknownError});
+
+  CreationClientCreationState.nothing()
+  : nameError = ClientCreationErrorType.nothing, phoneError = ClientCreationErrorType.nothing, unknownError = null;
+}
+
+enum ClientCreationErrorType {
+  clientExists,
+
+  noInput,
+
+  nothing
+}
